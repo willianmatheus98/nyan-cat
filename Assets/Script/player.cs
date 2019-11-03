a609ac9fd9cd676b7d4f3e8ef7aca0b0a0d4c06e
wilaman21 <williantecnico1998@gmail.com>
2019-11-03 14:52:20 -0300

    init

---
+++ Assets/Script/player.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class player : MonoBehaviour
+{   
+
+    private Rigidbody2D playerRb;
+    private SpriteRenderer playerSprite;
+    public float velocidade;
+    public bool flipX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody2D>();
+        playerSprite = GetComponent<SpriteRenderer>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        //botão esquerdo do mouse
+        if(Input.GetMouseButtonDown(0)){
+            velocidade *= -1;
+            flipX = !flipX;
+            playerSprite.flipX = flipX;
+        }
+
+        playerRb.velocity = new Vector2(velocidade, 0);
+    }
+
+    void OnCollisionEnter2D(Collision2D colisao){
+        
+        if(colisao.gameObject.tag == "meteoro"){
+            SceneManager.LoadScene("GameOver");
+        }
+    }
+}
