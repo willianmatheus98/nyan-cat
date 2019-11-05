debd4f3db4fa5888c2aa92e68233fb101205a2c9
sampaionicolly <42624711+sampaionicolly@users.noreply.github.com>
2019-11-05 13:40:14 -0300

    Fix Bugg

---
+++ Assets/Assets/Script/meteoro.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class meteoro : MonoBehaviour
+{   
+
+    private Rigidbody2D meteoroRb;
+    private int atrito;
+    public int  atritoMaximo, atritoMinimo, ponto;
+    public Vector3 posicao;
+    public GameObject meteoroPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        meteoroRb = GetComponent<Rigidbody2D>();
+        
+        atrito = Random.Range(atritoMinimo, atritoMaximo);
+
+        meteoroRb.drag = atrito;
+
+        posicao = transform.position;
+    }
+
+    void OnBecameInvisible(){    
+        Instantiate(this.gameObject, posicao, transform.localRotation);
+        pontuacao.pontos += ponto;
+        Destroy(this.gameObject);
+    }
+}
